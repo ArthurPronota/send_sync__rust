c225febd7f4a1268e824e349055e215ed863da3e
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-09-24 18:20:30 +0500

    Update thread print statements to dereference v_clone

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
                 let v_clone = v.clone() ;
                 move || {
                     println!("Thread: {}, v_clone: {:?}", i, v_clone) ;
-                    println!("*Thread: {}, v_clone: {:?}\n", i, *v_clone) ;
+                    println!("Thread: {}, *v_clone: {:?}\n", i, *v_clone) ;
                 }
             }) 
         ) ;
@@ -22,18 +22,18 @@ fn main() {
         h.join().unwrap() ;
     } /* Out:
 Thread: 0, v_clone: [1, 2, 3]
-*Thread: 0, v_clone: [1, 2, 3]
+Thread: 0, *v_clone: [1, 2, 3]
 
 Thread: 2, v_clone: [1, 2, 3]
-*Thread: 2, v_clone: [1, 2, 3]
+Thread: 2, *v_clone: [1, 2, 3]
 
 Thread: 1, v_clone: [1, 2, 3]
-*Thread: 1, v_clone: [1, 2, 3]
+Thread: 1, *v_clone: [1, 2, 3]
 
 Thread: 3, v_clone: [1, 2, 3]
-*Thread: 3, v_clone: [1, 2, 3]
+Thread: 3, *v_clone: [1, 2, 3]
 
 Thread: 4, v_clone: [1, 2, 3]
-*Thread: 4, v_clone: [1, 2, 3]    
+Thread: 4, *v_clone: [1, 2, 3]    
      */
 }
